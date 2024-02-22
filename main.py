14498ab27dd78b54345ac92519b3628e3f72986f
Ssyhaj <118054400+Ssyhaj@users.noreply.github.com>
2024-02-22 18:48:00 +0100

    changes to GUI and variable names
    expandable text frame
    changed colour of GUI
    added GUI icon
    proper naming at the top of the app
    clickable unread messages

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from threading import Thread
 import signal
 
 
-# wylogowanie użytkownika po wcisnięciu "ctrl + c"
 def keyboardInterruptHandler(signal, frame):
-
     if client_socket:
         dead = "DEAD"
         client_socket.send(dead.encode())
@@ -16,14 +14,9 @@ def keyboardInterruptHandler(signal, frame):
         top.destroy()
     os._exit(1)
 
-
-# obsługa sygnału "ctrl + c"
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
-
-# wylogowanie użytkownika po wciśnięciu "X"
 def on_closing(event=None):
-
     if client_socket:
         dead = "DEAD"
         client_socket.send(dead.encode())
@@ -31,45 +24,47 @@ def on_closing(event=None):
     top.destroy()
     os._exit(1)
 
-
-# GUI - przejście do okna konwersacji oraz wczytanie konwersacji
 def raise_messages(frame):
     frame.tkraise()
-    recipent = usr_name.get()
+    recipient = usr_name.get()
 
-    if recipent == "":
+    if recipient == "":
         return
 
-    if recipent in unread_messages.keys():
-        del unread_messages[recipent]
+    if recipient in unread_messages.keys():
+        del unread_messages[recipient]
 
-    labelvar.set(recipent)
+    labelvar.set(recipient)
     msg_list.delete(0, tkinter.END)
     mode = "r+"
-    if not(os.path.isfile("conversations/"+ my_username + "_" + recipent)):
+    if not(os.path.isfile("conversations/"+ my_username + "_" + recipient)):
         mode = "a+"
-    with open("conversations/" + my_username + "_" + recipent, mode) as f:
+    with open("conversations/" + my_username + "_" + recipient, mode) as f:
         lines = f.readlines()
         for line in lines:
             msg_list.insert(tkinter.END, line[:-1])  # usunięcie znaku nowej lini
         f.close()
 
-
-# GUI - przejście do okna Głównego
-def raise_menu(frame):
+def raise_menu(frame, usr_list):
     frame.tkraise()
     usr_name.set("")
     refresh_menu()
+    usr_list.bind("<Double-Button-1>", user_clicked)  # Przypisanie zdarzenia kliknięcia na liście użytkowników
 
-
-# GUI - odświeżenie okna Głównego w celu wyśletlenia nowych przychodzących wiadomości
 def refresh_menu():
     usr_list.delete(0, tkinter.END)
-    for user in unread_messages:
-        usr_list.insert(tkinter.END, str(user) + " : " + str(unread_messages[user]))
+    for user, unread_count in unread_messages.items():
+        usr_list.insert(tkinter.END, f"{user} ({unread_count})")
 
+def user_clicked(event):
+    selection = usr_list.curselection()
+    if selection:
+        index = selection[0]
+        username_with_count = usr_list.get(index)
+        username = username_with_count.split(" ")[0]  # Wyodrębnienie nazwy użytkownika
+        usr_name.set(username)  # Ustawienie nazwy użytkownika w polu tekstowym
+        raise_messages(messages_frame)  # Rozpoczęcie konwersacji z wybranym użytkownikiem
 
-# obsługa odbioru wiadomości
 def receive():
     while True:
         try:
@@ -84,7 +79,6 @@ def receive():
                 f.write(message + "\n")
                 f.close()
 
-            # Wyświetlenie informacji o przychodzących wiadomościach jeśli konwersacja z użytkownikiem wysyłającym nie jest aktualnie otwarta
             if sender != usr_name.get():
                 if sender not in unread_messages.keys():
                     unread_messages[sender] = 0
@@ -98,15 +92,14 @@ def receive():
             break
 
 
-# obsługa wysyłania wiadomości
 def send(event=None):
     msg = my_msg.get()
     if msg == "":
         return
     my_msg.set("")
-    recipent = usr_name.get()
-    # Przekształcenie wiadomości do postaci zrozumiałej dla serwera
-    msgf = "SEND " + recipent + " " + my_username + " " + msg
+    entry_field.config(width=10)
+    recipient = usr_name.get()
+    msgf = "SEND " + recipient + " " + my_username + " " + msg
     client_socket.send(msgf.encode())
 
     msg = "Ty: " + msg
@@ -114,39 +107,32 @@ def send(event=None):
 
     msg_list.see(tkinter.END)
 
-    with open("conversations/" + my_username + "_" + recipent, "a+") as f:
+    with open("conversations/" + my_username + "_" + recipient, "a+") as f:
         f.write(msg + "\n")
         f.close()
 
+hostname = "192.168.124.128" # ip serwera
+port = 1234          #port serwera
 
-# Główny program
+my_username = input("Username: ")
 
-IP = "192.168.80.130" # ip serwera
-PORT = 1234          #port serwera
-
-my_username = input("Username: ") # wczytanie loginu użytkownika
-
-# Utworzenie socketa
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Połączenie z serwerem
-client_socket.connect((IP, PORT))
+client_socket.connect((hostname, port))
 
-# Utworzenie folderu przechowującego konwersacje użytkowników
 if not os.path.exists('conversations'):
     os.mkdir('conversations')
 
-# Wysłanie informacji o zalagowaniu się użytkownika w postaci zrozumiałej dla serwera
 username = "LOGIN " + my_username
 client_socket.send(username.encode())
-
 
 def rgb_hack(rgb):
     return "#%02x%02x%02x" % rgb
 
 top = tkinter.Tk()
-top.title("GG")
+top.iconbitmap('icon.ico')
+top.title("Chat - " + my_username)
 
 usr_name = ""
 unread_messages = {}
@@ -154,54 +140,55 @@ unread_messages = {}
 messages_frame = tkinter.Frame(top)
 
 my_msg = tkinter.StringVar()
-my_msg.set("Czesc")
 
 scrollbar = tkinter.Scrollbar(messages_frame)
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
 msg_list.pack()
 
-
-messages_frame.grid(row=0, column=0)
+messages_frame.grid(row=0, column=0, sticky="nsew")  # sticky="nsew" pozwoli na rozciąganie się ramki
 messages_frame.config(bg=rgb_hack((220, 220, 220)))
 labelvar = tkinter.StringVar()
 label = tkinter.Label(messages_frame, textvariable=labelvar)
 label.config(bg=rgb_hack((220, 220, 220)))
 label.pack()
 
+def update_entry_width(event=None):
+    entry_width = len(my_msg.get()) + 5  # Adjust this value as needed for padding
+    entry_field.config(width=entry_width)
+
 entry_field = tkinter.Entry(messages_frame, textvariable=my_msg)
 entry_field.bind("<Return>", send)
-entry_field.pack()
+entry_field.bind("<KeyRelease>", update_entry_width)  # Bind the update function to key release event
+entry_field.pack(fill=tkinter.X)
 
-
-send_button = tkinter.Button(messages_frame, text="Wyślij", bg='black', fg='white', command=send)
+send_button = tkinter.Button(messages_frame, text="Wyslij", bg='blue', fg='white', command=send)
 send_button.pack()
-back_button = tkinter.Button(messages_frame, text="Menu",  bg='black', fg='white', command=lambda: raise_menu(menu_frame))
+back_button = tkinter.Button(messages_frame, text="Wyjdz z rozmowy",  bg='blue', fg='white', command=lambda: raise_menu(menu_frame, usr_list))
 back_button.pack()
 
 menu_frame = tkinter.Frame(top)
 usr_name = tkinter.StringVar()
 usr_name.set("Type name of user")
 
-
-menu_frame.grid(row=0, column=0)
+menu_frame.grid(row=0, column=0, sticky="nsew")  # sticky="nsew" pozwoli na rozciąganie się ramki
 menu_frame.config(bg=rgb_hack((220, 220, 220)))
 
 scrollbar2 = tkinter.Scrollbar(menu_frame)
 usr_list = tkinter.Listbox(menu_frame, height=15, width=50, yscrollcommand=scrollbar2.set)
 scrollbar2.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-usr_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+usr_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
 usr_list.pack()
 
-label_menu = tkinter.Label(menu_frame, text="Nazwa użytkownika:")
+label_menu = tkinter.Label(menu_frame, text="Nazwa uzytkownika:")
 label_menu.config(bg=rgb_hack((220, 220, 220)))
 label_menu.pack()
 
 name_field = tkinter.Entry(menu_frame, textvariable=usr_name)
-name_field.pack()
+name_field.pack(fill=tkinter.X)  # fill=tkinter.X pozwoli na rozciąganie się pola tekstowego wzdłuż osi X
 
-name_button = tkinter.Button(menu_frame, text="Zatwierdź", bg='black', fg='white',command=lambda: raise_messages(messages_frame))
+name_button = tkinter.Button(menu_frame, text="Potwierdz", bg='blue', fg='white',command=lambda: raise_messages(messages_frame))
 name_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
@@ -209,5 +196,10 @@ top.protocol("WM_DELETE_WINDOW", on_closing)
 receive_thread = Thread(target=receive)
 receive_thread.start()
 
-raise_menu(menu_frame)
+raise_menu(menu_frame, usr_list)
+
+# Konfiguracja rozciągania się ramki w oknie głównym
+top.grid_rowconfigure(0, weight=1)
+top.grid_columnconfigure(0, weight=1)
+
 tkinter.mainloop()
